eee1b09e5df5547f9edef86e22caefa13691f4ab
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2021-05-27 03:15:36 +0700

    feat: add simulation to call different function on the same package

--- main.go
+++ main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades" // First init var using :=
-	card = "Five of Diamonds"
+	//card := "Ace of Spades" // First init var using :=
+	//card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+	printState()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
