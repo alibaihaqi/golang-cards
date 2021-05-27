ecbbf9f3971a7c17c8edea4d88244d0efbd729e2
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2021-05-27 18:31:55 +0700

    feat: add newDeck function to create and get list of cards

--- deck.go
+++ deck.go
@@ -8,6 +8,22 @@ import "fmt"
 */
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cardName := value + " of " + suit
+			cards = append(cards, cardName)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
